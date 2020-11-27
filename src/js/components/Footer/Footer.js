deae003fe7144e358bc61f91451025da2d07e527
lelislois Ben <samirbenzada@gmail.com>
2020-11-27 18:05:50 -0500

    updated

--- src/js/components/Footer/Footer.js
+++ src/js/components/Footer/Footer.js
@@ -3,27 +3,11 @@ import styles from './Footer.module.css';
 
 const Footer = () => {
     return (
-        <footer className={styles.footer}>
+      <footer className={styles.footer}>
          <div className="container">
-            <div className="agileits-contact-addrss">
-               <div className="row top-gap">
-                  <div className="col-md-7 header-side">
-                     <p> 
-                        © 2019 iWash. All Rights Reserved | Design by Samir
-                     </p>
-                  </div>
-                  <div className="col-md-5 header-side">
-                     <div className={styles.socialBtn}>
-                        <ul>
-                           <li className="mr-2"><a href="/"><span className="fab fa-facebook-f"></span></a></li>
-                           <li className="mr-2"><a href="/"><span className="fab fa-google"></span></a></li>
-                           <li className="mr-2"><a href="/"><span className="fab fa-twitter"></span></a></li>
-                           <li className="mr-2"><a href="/"><span className="fab fa-whatsapp"></span></a></li>
-                        </ul>
-                     </div>
-                  </div>
-               </div>
-            </div>
+            <p> 
+               © 2020 Haedquarter. All Rights Reserved | Design by Samir Benzada
+            </p>
          </div>
       </footer>
     );
