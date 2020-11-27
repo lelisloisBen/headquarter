fbf530715a6fa331284b1d1de119cfe6c81f8884
lelislois Ben <samirbenzada@gmail.com>
2020-11-27 17:55:03 -0500

    updated

--- src/js/components/Tab1/Tab1.js
+++ src/js/components/Tab1/Tab1.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 // import styles from './Tab1.module.css';
 
 const Tab1 = () => {
     return (
-        <div className="container" >
+        <div className="container py-5" >
             <h1>Consultant informations</h1>
 
-            <div class="list-group">
-                <a href="#" class="list-group-item list-group-item-action active">
-                    Cras justo odio
-                </a>
-                <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
-                <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
-                <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-                <a href="#" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">Vestibulum at eros</a>
+            <div className="list-group">
+                <Link to="/" className="list-group-item list-group-item-action">Name Lastname </Link>
+                <Link to="/" className="list-group-item list-group-item-action">Name Lastname </Link>
+                <Link to="/" className="list-group-item list-group-item-action">Name Lastname </Link>
+                <Link to="/" className="list-group-item list-group-item-action">Name Lastname </Link>
+                <Link to="/" className="list-group-item list-group-item-action">Name Lastname </Link>
+                <Link to="/" className="list-group-item list-group-item-action">Name Lastname </Link>
+                <Link to="/" className="list-group-item list-group-item-action">Name Lastname </Link>
+                <Link to="/" className="list-group-item list-group-item-action">Name Lastname </Link>
+                <Link to="/" className="list-group-item list-group-item-action">Name Lastname </Link>
+                <Link to="/" className="list-group-item list-group-item-action">Name Lastname </Link>
+                <Link to="/" className="list-group-item list-group-item-action">Name Lastname </Link>
+                <Link to="/" className="list-group-item list-group-item-action">Name Lastname </Link>
             </div>
         </div>
     )
