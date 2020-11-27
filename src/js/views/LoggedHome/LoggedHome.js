d1d458b65d5ee2caba93fef522e02fa095590835
lelislois Ben <samirbenzada@gmail.com>
2020-11-27 16:28:18 -0500

    updated

--- src/js/views/LoggedHome/LoggedHome.js
+++ src/js/views/LoggedHome/LoggedHome.js
@@ -13,20 +13,72 @@ const LoggedHome = () => {
         
             <ul className="nav nav-tabs" id="myTab" role="tablist">
                 <li className="nav-item" role="presentation">
-                    <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+                    <a  className={["nav-link active", styles.tab1Color].join(' ')} 
+                        id="consultant-tab"
+                        data-toggle="tab" 
+                        href="#consultant" 
+                        role="tab" 
+                        aria-controls="consultant" 
+                        aria-selected="true"
+                    >
+                        Consultant
+                    </a>
                 </li>
                 <li className="nav-item" role="presentation">
-                    <a className="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+                    <a  className={["nav-link", styles.tab2Color].join(' ')} 
+                        id="profile-tab" 
+                        data-toggle="tab" 
+                        href="#profile" 
+                        role="tab" 
+                        aria-controls="profile" 
+                        aria-selected="false"
+                    >
+                        Profile
+                    </a>
                 </li>
                 <li className="nav-item" role="presentation">
-                    <a className="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+                    <a  className={["nav-link", styles.tab3Color].join(' ')} 
+                        id="contact-tab" 
+                        data-toggle="tab" 
+                        href="#contact" 
+                        role="tab" 
+                        aria-controls="contact" 
+                        aria-selected="false"
+                    >
+                        Contact
+                    </a>
                 </li>
             </ul>
 
             <div className="tab-content" id="myTabContent">
-                <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">tab 1</div>
-                <div className="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">tab 2</div>
-                <div className="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">tab 3</div>
+
+                <div 
+                    className="tab-pane fade show active" 
+                    id="consultant" 
+                    role="tabpanel" 
+                    aria-labelledby="consultant-tab"
+                >
+                    consultant informations
+                </div>
+
+                <div 
+                    className="tab-pane fade" 
+                    id="profile" 
+                    role="tabpanel" 
+                    aria-labelledby="profile-tab"
+                >
+                    tab 2
+                </div>
+
+                <div 
+                    className="tab-pane fade" 
+                    id="contact" 
+                    role="tabpanel" 
+                    aria-labelledby="contact-tab"
+                >
+                    tab 3
+                </div>
+
             </div> 
 
         </section>
