e24f76847b244e57a73b37e24f2f6d1f8d79aada
lelislois Ben <samirbenzada@gmail.com>
2020-12-02 01:06:32 -0500

    updated

--- src/js/views/ConsultantProfile/ConsultantProfile.js
+++ src/js/views/ConsultantProfile/ConsultantProfile.js
@@ -23,43 +23,43 @@ const ConsultantProfile = (props) => {
         <div className={styles.section}>
             <div className="container mx-auto">
 
-                <div class="card text-white bg-primary mb-3">
-                    <div class="card-header text-center"> <h1>Consultant Profile</h1> </div>
-                    <div class="card-body">
-                        <h5 class="card-title text-center"> 
+                <div className="card text-white bg-primary mb-3">
+                    <div className="card-header text-center"> <h1>Consultant Profile</h1> </div>
+                    <div className="card-body">
+                        <h5 className="card-title text-center"> 
                             <b>{C.firstname} {C.lastname}</b> 
                             <Link to="/" className={["float-right", styles.editProfile].join(' ')}>
-                                <span ><i class="fas fa-user-edit"></i></span>
+                                <span ><i className="fas fa-user-edit"></i></span>
                             </Link>
                         </h5>
-                        <ul class="list-group list-group-flush" style={{color: "black"}}>
+                        <ul className="list-group list-group-flush" style={{color: "black"}}>
 
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Birthdate: </b> {C.birthdate}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Gender: </b> {C.gender}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Phone: </b> {C.phone}
                             </li>
                             
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Address: </b> <br/>
                                 {C.address} <br/>
                                 {C.city}, {C.state} {C.zipCode}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Personal Email: </b> {C.emailPerso}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Work Email: </b> {C.emailWork}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Skype ID: </b> {C.skype}
                             </li>
 
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Bank Name: </b> {C.bankName} 
                                 <br/>
                                 <b>Account Number: </b> {C.account} 
--- src/js/views/InterviewDetails/InterviewDetails.js
+++ src/js/views/InterviewDetails/InterviewDetails.js
@@ -20,78 +20,78 @@ const InterviewDetails = (props) => {
         <div className={styles.section}>
             <div className="container mx-auto">
 
-                <div class="card text-white bg-warning mb-3">
-                    <div class="card-header text-center"> <h1>Interview Detail</h1> </div>
-                    <div class="card-body">
-                        <h5 class="card-title text-center"> 
+                <div className="card text-white bg-warning mb-3">
+                    <div className="card-header text-center"> <h1>Interview Detail</h1> </div>
+                    <div className="card-body">
+                        <h5 className="card-title text-center"> 
                             <b>{I.firstname} {I.lastname}</b> 
                         </h5>
-                        <ul class="list-group list-group-flush" style={{color: "black"}}>
+                        <ul className="list-group list-group-flush" style={{color: "black"}}>
                         
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>email: </b> {I.email}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Interview Time: </b> {I.time}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Client: </b> {I.client}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Vendor: </b> {I.vendor}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Implementation Partner: </b> {I.implementationpartner}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Mode: </b> {I.mode}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Call Type: </b> {I.calltype}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>First Assistant: </b> {I.assist1}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Second Assistant: </b> {I.assist2}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Sale Associate: </b> {I.saleassociate}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Manager: </b> {I.manager}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Live Coding: </b> {I.livecoding}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Position Title: </b> {I.positiontitle}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Job Description: </b> 
                                 <br/>
                                 <br/>
                                 {I.jobdescription}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Project Duration: </b> {I.projectduration}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Project Location: </b> {I.projectlocation}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Client Website: </b> {I.clientwebsite}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Vendor Website: </b> {I.vendorwebsite}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Interviewer Name: </b> {I.interviewername}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Interviewer LinkedIn: </b> {I.interviewerlinkedIn}
                             </li>
-                            <li class="list-group-item">
+                            <li className="list-group-item">
                                 <b>Vendor Notes: </b> {I.vendornotes}
                             </li>
 
