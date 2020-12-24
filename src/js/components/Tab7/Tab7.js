e6607347a8c0803be0f4e7dc9ade55a941511fe2
lelislois Ben <samirbenzada@gmail.com>
2020-12-23 22:31:23 -0500

    updated

--- src/js/components/Tab7/Tab7.js
+++ src/js/components/Tab7/Tab7.js
@@ -1,12 +1,56 @@
-import React, { useContext } from 'react';
-// import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
 import { UserContext } from '../../../UserContext';
-// import styles from './Tab1.module.css';
+import swal from 'sweetalert';
 
 const Tab7 = () => {
 
+    const {backen_url} = useContext(UserContext);
     const {consultantData} = useContext(UserContext);
+    const [student, setStudent] = useState('');
+    
+    const splitedStudent = student.split(',');
+    const firstname = splitedStudent[0];
+    const lastname = splitedStudent[1];
+    const email = splitedStudent[2];
     const studentPassword = "An4th!ng";
+
+    const allData = JSON.stringify({
+        "email": email,
+        "firstname": firstname,
+        "lastname": lastname,
+        "password": studentPassword
+    });
+
+    const submitForm = () => {
+        if ( !student || !firstname || !lastname || !email ) {
+            swal("Something Went Wrong!", "Please select a consultant !", "error", {
+                button: "OK",
+            });
+        } else {
+            fetch(backen_url+'/registerDatavaultCourses', {
+                method: 'POST',
+                body: allData,
+                cors: 'no-cors',
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+                }).then(res => res.json())
+                .then(response => {
+                    console.log(JSON.stringify(response));
+                    swal("SAVED SUCCESSFULLY!", "Student saved and can access courses", "success", {
+                        button: "Go teach now!",
+                    }).then(() => {
+                            window.location.reload();
+                        });
+                })
+                .catch(error => {
+                    swal("Something Went Wrong!", JSON.stringify("error: => "+ error), "error", {
+                        button: "OK",
+                    })
+                });
+        }
+    }
+
 
     return (
         <div className="container py-5" >
@@ -15,8 +59,8 @@ const Tab7 = () => {
             <div className="form-row">
                 <div className="form-group col-md-6">
                     <label htmlFor="inputConsultant">Consultant List</label>
-                    <select className="form-control" id="inputConsultant" onChange={(e) => setc_Consultant(e.target.value)}>
-                        <option value="" defaultValue="selected">{c_Consultant}</option>
+                    <select className="form-control" id="inputConsultant" onChange={(e) => setStudent(e.target.value)}>
+                        <option value="" defaultValue="selected">Select a Consultant</option>
                         {!consultantData ? "Empty list of consultants..." : consultantData.map((item, index) => {
                             return (
                                 <option key={index} value={item.firstname+','+item.lastname+','+item.emailWork} >{item.firstname + ' ' + item.lastname}</option>
@@ -26,6 +70,14 @@ const Tab7 = () => {
                     </select>
                 </div>
             </div>
+
+            <button 
+                type="submit" 
+                className="btn btn-success"
+                onClick={submitForm}
+            >
+                Save Student for Courses
+            </button>
 
         </div>
     )
