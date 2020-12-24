8db6ca1045dda40437fb6aae96b59bba0af0b307
lelislois Ben <samirbenzada@gmail.com>
2020-12-23 22:15:08 -0500

    updated

--- src/js/components/Tab7/Tab7.js
+++ src/js/components/Tab7/Tab7.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 // import { Link } from 'react-router-dom';
-// import { UserContext } from '../../../UserContext';
+import { UserContext } from '../../../UserContext';
 // import styles from './Tab1.module.css';
 
 const Tab7 = () => {
 
-    // const {interviewsData} = useContext(UserContext);
-
-    // console.log(consultantData);
+    const {consultantData} = useContext(UserContext);
+    const studentPassword = "An4th!ng";
 
     return (
         <div className="container py-5" >
             <h1>Add Student form</h1>
+
+            <div className="form-row">
+                <div className="form-group col-md-6">
+                    <label htmlFor="inputConsultant">Consultant List</label>
+                    <select className="form-control" id="inputConsultant" onChange={(e) => setc_Consultant(e.target.value)}>
+                        <option value="" defaultValue="selected">{c_Consultant}</option>
+                        {!consultantData ? "Empty list of consultants..." : consultantData.map((item, index) => {
+                            return (
+                                <option key={index} value={item.firstname+','+item.lastname+','+item.emailWork} >{item.firstname + ' ' + item.lastname}</option>
+                            )
+                        })
+                        }
+                    </select>
+                </div>
+            </div>
 
         </div>
     )
